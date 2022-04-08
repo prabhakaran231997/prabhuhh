e8c6f223f84194465f15d479531fa248c63370e4
prabhakaran231997 <prabhakarancivil23@gmail.com>
2022-04-08 07:46:28 +0530

    done

---
+++ src/com/people/AllClassOfGit.java
@@ -0,0 +1,15 @@
+package com.people;
+
+public class AllClassOfGit {
+	public void add1() {
+		System.out.println("add1");
+	}
+	public void add2() {
+		System.out.println("add2");
+	}
+	public static void main(String[] args) {
+		AllClassOfGit ss = new AllClassOfGit();
+		ss.add1();
+		ss.add2();
+	}
+}
